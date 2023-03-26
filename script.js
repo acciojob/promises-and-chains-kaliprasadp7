b58e4ff37e1b89610746aa86636392e437fe9863
Kali Prasad Panda <80939822+kaliprasadp7@users.noreply.github.com>
2023-03-26 15:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+async function formdata(form) {
+	
+	let age = new Promise((resolve, reject) => {
+    setTimeout(() => {
+		let condition = document.getElementById("age").value;
+		let name = document.getElementById("name").value;
+        if(condition >= 18){
+            resolve(alert("Welcome, "+name+". You can vote."))
+        }else{
+			reject(alert("Oh sorry "+name+". You aren't old enough."))
+		}
+    }, 4000);
+})
+
+	// alert(age)
+}
